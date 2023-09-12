01ca2bc481f79230ddeb92883fe53e7010bf207e
devMonkey87 <jlandetemunoz@icloud.com>
2023-09-12 20:38:59 +0200

    Initial Commit

---
+++ shopping-list/shopping_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  shopping_listApp.swift
+//  shopping-list
+//
+//  Created by Jose Antonio Landete Muñoz on 12/9/23.
+//
+
+import SwiftUI
+
+@main
+struct shopping_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
