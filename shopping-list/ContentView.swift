9afc8c919511f3ed1afc252bea18da6505374686
devMonkey87 <jlandetemunoz@icloud.com>
2023-09-15 19:25:56 +0200

    initial commit

--- shopping-list/ContentView.swift
+++ shopping-list/ContentView.swift
@@ -1,26 +1,41 @@
-//
-//  ContentView.swift
-//  shopping-list
-//
-//  Created by Jose Antonio Landete Muñoz on 12/9/23.
-//
-
 import SwiftUI
 
+var globalProperty: Int = 0
+
+func useGlobalProperties() {
+    globalProperty += 1
+}
+
 struct ContentView: View {
+    @State private var labelText = "0"
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+                .foregroundColor(.red)
+            Text("My first iOS app!").bold(true)
+            
+            Button("Clicame porfi") {
+                useGlobalProperties()
+                labelText = "\(globalProperty)"
+            }
+            .padding(.top, 10.0)
+            
+            
+            
+            Label("Contador", systemImage: "\(labelText).circle")
+                .padding(.top, 10.0)
         }
-        .padding()
+        .frame(width: 500, height: 1000) // repasar cuantos son las constantes que puedo usar
+        .background(Color.blue)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+/*
+ struct ContentView_Previews: PreviewProvider {
+ static var previews: some View {
+ ContentView()
+ }
+ }*/
+ 
