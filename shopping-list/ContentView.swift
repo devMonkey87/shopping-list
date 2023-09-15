5814b2e064a52ee4ffc4bd0ddc440d529a604163
devMonkey87 <jlandetemunoz@icloud.com>
2023-09-15 20:29:48 +0200

    initial commit

--- shopping-list/ContentView.swift
+++ shopping-list/ContentView.swift
@@ -8,7 +8,7 @@ func useGlobalProperties() {
 
 struct ContentView: View {
     @State private var labelText = "0"
-
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -16,22 +16,33 @@ struct ContentView: View {
                 .foregroundColor(.red)
             Text("My first iOS app!").bold(true)
             
-            Button("Clicame porfi") {
+            Button(action:{
                 useGlobalProperties()
                 labelText = "\(globalProperty)"
+            }) {
+                Text("Clicame porfi papa")
+                    .padding(.vertical, 10)
+                    .padding(.horizontal, 20)
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
             }
-            .padding(.top, 10.0)
             
-            
-            
-            Label("Contador", systemImage: "\(labelText).circle")
+            Label("Quesitos", systemImage: "\(labelText).circle")
                 .padding(.top, 10.0)
+            
+            Text("🐭") // Agrega el emoji del ratón a la derecha del Label
+                .font(.system(size: 100))
         }
+        
+        
+        
+        
         .frame(width: 500, height: 1000) // repasar cuantos son las constantes que puedo usar
         .background(Color.blue)
     }
+    
 }
-
 /*
  struct ContentView_Previews: PreviewProvider {
  static var previews: some View {
